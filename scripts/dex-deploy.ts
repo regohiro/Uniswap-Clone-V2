d42e6df3be4f8d98222a32a4c28d882d750ba54b
REGO350 <riokanehiro228pa@gmail.com>
2021-08-14 22:20:37 +0900

    [add] deploy program

---
+++ scripts/dex-deploy.ts
@@ -0,0 +1,41 @@
+import { Uni } from './../typechain/Uni.d';
+import { Link } from './../typechain/Link.d';
+import { Dai } from './../typechain/Dai.d';
+
+import { ethers } from "hardhat";
+import { verify, deployToLiveNetwork, setDefaultSigner, deployer, getContractInstance } from '../utils';
+ 
+async function main() {
+  //When called, it will print receipt and verify to BscScan
+  deployToLiveNetwork();
+
+  //Set contract signer (owner)
+  const signers = await ethers.getSigners();
+  const owner = signers[0];
+  setDefaultSigner(owner);
+
+  //Get token contract instance
+  const dai = await getContractInstance("Dai") as Dai;
+  const link = await getContractInstance("Link") as Link;
+  const uni = await getContractInstance("Uni") as Uni;
+
+  //Set dex contract args
+  const tokenAddr = [dai.address, link.address, uni.address];
+  const tokenPriceAddr = [
+    "0x22B58f1EbEDfCA50feF632bD73368b2FdA96D541",
+    "0x3Af8C569ab77af5230596Acf0E8c2F9351d24C38",
+    "0x17756515f112429471F86f98D5052aCB6C47f6ee",
+  ];
+
+  //Deploy dex contract
+  await deployer("Dex", tokenAddr, tokenPriceAddr);
+}
+
+//Excute deploy
+main()
+  .then(verify)
+  .then(() => process.exit(0))
+  .catch((error: Error) => {
+    console.error(error);
+    process.exit(1);
+  });
---
+++ scripts/tokens-deploy.ts
@@ -0,0 +1,32 @@
+
+import { ethers } from "hardhat";
+import { verify, deployToLiveNetwork, setDefaultSigner, deployer, toWei } from '../utils';
+ 
+async function main() {
+  //When called, it will print receipt and verify to BscScan
+  deployToLiveNetwork();
+
+  //Set contract signer (owner)
+  const signers = await ethers.getSigners();
+  const owner = signers[0];
+  setDefaultSigner(owner);
+
+  //Set Token contract args
+  const daiTotalSupply = toWei(10**10);
+  const linkTotalSupply = toWei(10**8);
+  const uniTotalSupply = toWei(10**8)
+
+  //Deploy token contracts
+  await deployer("Dai", daiTotalSupply);
+  await deployer("Link", linkTotalSupply);
+  await deployer("Uni", uniTotalSupply);
+}
+
+//Excute deploy
+main()
+  .then(verify)
+  .then(() => process.exit(0))
+  .catch((error: Error) => {
+    console.error(error);
+    process.exit(1);
+  });
